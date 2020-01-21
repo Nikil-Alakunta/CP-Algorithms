d0e0c6bc7f89ad1ac37d91b481be056691e69323
Nikil99 <nikilalakunta09031999@gmail.com>
2020-01-21 15:39:23 +0530

    Create UVa_1230_MODEX.cpp
    https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=24&page=show_problem&problem=3671

---
+++ UVa_1230_MODEX.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+
+using namespace std;
+
+long long binpow(long long x,long long y,long long n){
+	x%=n;
+    long long res=1;
+    while(y>0){
+    	if (y&1){
+    		res=res*x%n;
+    	}
+    	x=x * x % n;
+    	y>>=1;
+    }
+    return res;
+}
+ 
+
+int main() 
+{ 
+	int c,end;
+	long long x,y,n;
+	cin>>c;
+	for(int i=0;i<c;++i){
+		cin>>x>>y>>n;
+		cout<<binpow(x,y,n)<<endl;
+	}
+	cin>>end;
+	return 0; 
+} 
